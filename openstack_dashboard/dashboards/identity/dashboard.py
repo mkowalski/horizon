5564b838924f199f42b01f3c25f20b84501c8c87
Jose Castro Leon <jose.castro.leon@cern.ch>
2016-07-21 13:45:23 +0200

    Disable idenity dashboard

--- openstack_dashboard/dashboards/identity/dashboard.py
+++ openstack_dashboard/dashboards/identity/dashboard.py
@@ -22,6 +22,7 @@ class Identity(horizon.Dashboard):
     name = _("Identity")
     slug = "identity"
     default_panel = 'projects'
+    permissions = ('openstack.roles.admin',)
 
 
 horizon.register(Identity)
