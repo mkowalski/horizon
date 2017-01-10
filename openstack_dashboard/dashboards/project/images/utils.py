f99d96d2cebc805270849fbcb2fccbf0593a3dbe
Mateusz Kowalski <mateusz.kowalski@cern.ch>
2017-01-10 09:00:38 +0100

    Make get_available_images() return all the images available
    This patch fixes get_available_images() not returning images which
    are shared with a project. Old version returns only public images
    or project-owned ones, missing images which were shared with
    the project.
    
    Closes-bug: #1655259
    
    Change-Id: I2a264d83f3e2b3f10c1e00ac7e88db29bb34bd02

--- openstack_dashboard/dashboards/project/images/utils.py
+++ openstack_dashboard/dashboards/project/images/utils.py
@@ -19,62 +19,32 @@ from openstack_dashboard.api import glance
 
 
 def get_available_images(request, project_id=None, images_cache=None):
-    """Returns a list of images that are public or owned by the given
-    project_id. If project_id is not specified, only public images
-    are returned.
+    """Returns a list of images that are available in a certain context.
+    Project_id is here only for backwards compatibility. The function shall return
+    public, private and shared images.
 
     :param images_cache: An optional dict-like object in which to
-     cache public and per-project id image metadata.
+    cache image metadata.
 
     """
+
     if images_cache is None:
         images_cache = {}
-    public_images = images_cache.get('public_images', [])
-    images_by_project = images_cache.get('images_by_project', {})
-    if 'public_images' not in images_cache:
-        public = {"is_public": True,
-                  "status": "active"}
+
+    all_images = images_cache.get('all_images', [])
+
+    if 'all_images' not in images_cache:
         try:
             images, _more, _prev = glance.image_list_detailed(
-                request, filters=public)
-            [public_images.append(image) for image in images]
-            images_cache['public_images'] = public_images
+                request)
+            [all_images.append(image) for image in images]
+            images_cache['all_images'] = images
         except Exception:
             exceptions.handle(request,
-                              _("Unable to retrieve public images."))
+                              _("Unable to retrieve images."))
 
-    # Preempt if we don't have a project_id yet.
-    if project_id is None:
-        images_by_project[project_id] = []
-
-    if project_id not in images_by_project:
-        owner = {"property-owner_id": project_id,
-                 "status": "active"}
-        try:
-            owned_images, _more, _prev = glance.image_list_detailed(
-                request, filters=owner)
-            images_by_project[project_id] = owned_images
-        except Exception:
-            owned_images = []
-            exceptions.handle(request,
-                              _("Unable to retrieve images for "
-                                "the current project."))
-    else:
-        owned_images = images_by_project[project_id]
-
-    if 'images_by_project' not in images_cache:
-        images_cache['images_by_project'] = images_by_project
-
-    images = owned_images + public_images
-
-    image_ids = []
-    final_images = []
-    for image in images:
-        if image.id not in image_ids and \
-                image.container_format not in ('aki', 'ari'):
-            image_ids.append(image.id)
-            final_images.append(image)
-    return final_images
+    return [image for image in all_images
+            if image.container_format not in ('aki', 'ari')]
 
 
 def image_field_data(request, include_empty_option=False):
